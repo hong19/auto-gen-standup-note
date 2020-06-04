1ec568e76f6e278cd9a5be5966fe1625243f151c
David Yu <david@kloudless.com>
2020-06-04 10:27:19 +0800

    fix: make sure getArticle use the token retrieved from logIn

--- src/app.js
+++ src/app.js
@@ -8,52 +8,47 @@ const client = new bot({
   protocol: config.protocol,           // Wikipedia now enforces HTTPS
   server: config.server,  // host name of MediaWiki-powered site
   path: config.path,                  // path to api.php script
-  debug: true                 // is more verbose when set to true
+  debug: true,                 // is more verbose when set to true
+  username: config.username,
+  password: config.password
 });
 
-function handleResponse(err, data) {
-  // error handling
+client.logIn( function (err) {
   if (err) {
     console.error(err);
-  } else {
-    console.log(data);
+    return;
   }
-}
 
-client.logIn(config.username, config.password, handleResponse);
+  client.getArticle('Meeting_Minutes', function (err, MeetingMinutesContent) {
+    // error handling
+    if (err) {
+      console.error(err);
+    } else {
+      let {title, index} = regex.findLastTwStandUpTitle(MeetingMinutesContent);
+      title = title.replace(/ /g, '_');
 
-client.getArticle('Meeting_Minutes', function (err, MeetingMinutesContent) {
-  // error handling
-  if (err) {
-    console.error(err);
-  } else {
-    let {title, index} = regex.findLastTwStandUpTitle(MeetingMinutesContent);
-    title = title.replace(/ /g, '_');
+      client.getArticle(title, function (err, data) {
+        if (err) {
+          console.error(err);
+        } else {
+          const cleanedContent = regex.cleanDoneSection(data);
+          const todayTitle = utils.generateTodayTitle();
+          const updatedMeetingMinutesContent = regex.generateNewMeetingMinutes(MeetingMinutesContent, index, todayTitle)
+          client.edit('Meeting_Minutes', updatedMeetingMinutesContent, null, false, function (err, data) {
+            if (err) {
+              console.log(err);
+            } else {
+              client.edit(todayTitle.replace(/ /g, '_'), cleanedContent, null, false, function (err, data) {
+                if (err) {
+                  console.log(err);
+                } else {
 
-    client.getArticle(title, function (err, data) {
-      if (err) {
-        console.error(err);
-      } else {
-        const cleanedContent = regex.cleanDoneSection(data);
-        const todayTitle = utils.generateTodayTitle();
-        const updatedMeetingMinutesContent = regex.generateNewMeetingMinutes(MeetingMinutesContent, index, todayTitle)
-        client.edit('Meeting_Minutes', updatedMeetingMinutesContent, null, false, function (err, data) {
-          if (err) {
-            console.log(err);
-          } else {
-            client.edit(todayTitle.replace(/ /g, '_'), cleanedContent, null, false, function (err, data) {
-              if (err) {
-                console.log(err);
-              } else {
-
-              }
-            });
-          }
-        })
-      }
-    });
-  }
+                }
+              });
+            }
+          })
+        }
+      });
+    }
+  });
 });
-
-
-
